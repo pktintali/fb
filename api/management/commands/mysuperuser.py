9630ce459eab92e4e846b21490f893f49a0c2efb
Pradeep Kumar <pktintali2@users.noreply.github.com>
2022-10-01 17:54:25 +0530

    new super user

--- api/management/commands/mysuperuser.py
+++ api/management/commands/mysuperuser.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin2').exists():
-            User.objects.create_superuser('admin2',
-                                          'admin2@domain.com',
+        if not User.objects.filter(username='pktintali').exists():
+            User.objects.create_superuser('pktintali',
+                                          'pktintali@gmail.com',
                                           '@@4444@@')
