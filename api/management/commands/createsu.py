d6cfb915bdee981ccf35b2f1d399f9a488311b7d
Pradeep Kumar <pktintali2@users.noreply.github.com>
2022-10-01 17:47:30 +0530

    superuser

--- api/management/commands/createsu.py
+++ api/management/commands/createsu.py
@@ -1,5 +1,11 @@
-from api.models import User
-def handle(self, *args, **options):
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@domain.com", "@@4444@@")
-        self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin',
+                                          'admin@domain.com',
+                                          '@@4444@@')
