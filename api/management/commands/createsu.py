9d1cdf4aaf0f460f977e9215f916b1e26c4bda54
Pradeep Kumar <pktintali2@users.noreply.github.com>
2022-10-01 17:48:13 +0530

    superuser

--- api/management/commands/createsu.py
+++ api/management/commands/createsu.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin',
-                                          'admin@domain.com',
+        if not User.objects.filter(username='admin2').exists():
+            User.objects.create_superuser('admin2',
+                                          'admin2@domain.com',
                                           '@@4444@@')
